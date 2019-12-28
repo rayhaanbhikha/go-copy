b2fb152fe48f614eaa66611ef96ff9719255e9e1
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-12-28 22:00:14 +0000

    todo comments added

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 func main() {
 	fmt.Println("hello world")
 
+	// check if aws creds are provided.
+
+	// TODO: scan input for aws access creds.
+	// TODO: or path to file for aws creds.
+
+	// install s3 util.
+
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
