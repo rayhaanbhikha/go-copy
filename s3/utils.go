312c7d740da49e354304ddf770d95c1053111af2
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-12-29 15:08:52 +0000

    check bucket exists separated into utils

---
+++ s3/utils.go
@@ -0,0 +1,27 @@
+package s3
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+// HandlerErr ... logs error
+func HandlerErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// CheckBucketExists ... checks if a bucket exists
+func CheckBucketExists(bucketName string, s3Instance *s3.S3) bool {
+	result, err := s3Instance.ListBuckets(&s3.ListBucketsInput{})
+	HandlerErr(err)
+	for _, bucket := range result.Buckets {
+		if aws.StringValue(bucket.Name) == bucketName {
+			return true
+		}
+	}
+	return false
+}
