ebfe54f49c8e4990cceb72ca00ef0e4861d06e63
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2019-12-29 15:08:32 +0000

    refactored s3 upload code.

--- s3/upload.go
+++ s3/upload.go
@@ -2,7 +2,6 @@ package s3
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -10,12 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
-
-func handlerErr(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 const (
 	region   = "eu-west-2"
@@ -31,43 +24,32 @@ var (
 	s3Instance = s3.New(sess)
 )
 
-func checkBucketExists() bool {
-	result, err := s3Instance.ListBuckets(&s3.ListBucketsInput{})
-	handlerErr(err)
-	for _, bucket := range result.Buckets {
-		if aws.StringValue(bucket.Name) == myBucket {
-			return true
-		}
-	}
-	return false
-}
-
 func createBucket() {
 	fmt.Printf("creating bucket %s\n...", myBucket)
 	result, err := s3Instance.CreateBucket(&s3.CreateBucketInput{
 		Bucket: aws.String(myBucket),
 	})
-	handlerErr(err)
+	HandlerErr(err)
 	fmt.Println(result)
 }
 
 // Upload ... accepts an array of bytes.
-func Upload(data []byte) {
+func Upload(data string) {
 	fmt.Println("upload method was called")
-	bucketExists := checkBucketExists()
+	bucketExists := CheckBucketExists(myBucket, s3Instance)
 	if !bucketExists {
 		createBucket()
 	}
 
-	myBody := strings.NewReader("this is another test")
+	// myBody := strings.NewReader("this is another test")
 	uploader := s3manager.NewUploader(sess)
 
 	_, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(myBucket),
 		Key:    aws.String(myKey),
-		Body:   myBody,
+		Body:   strings.NewReader(data),
 	})
-	handlerErr(err)
+	HandlerErr(err)
 
 	fmt.Println("object added")
 }
